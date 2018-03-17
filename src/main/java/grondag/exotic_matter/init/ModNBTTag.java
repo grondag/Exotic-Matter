acee6c1958a96930c69a36d33495f1aeb3bdf9b7
grondag <grondag@gmail.com>
2018-03-17 09:13:58 -0700

    Move to library mod

---
+++ src/main/java/grondag/exotic_matter/init/ModNBTTag.java
@@ -0,0 +1,17 @@
+package grondag.exotic_matter.init;
+
+/**
+ * Provides concise and consistent (albeit human-unfriendly) tags for serialization. 
+ * DO NOT MODIFY ORDER OR REMOVE VALUES UNLESS PUBLISHING A WORLD-BREAKING RELEASE.
+ * 
+ * @author grondag
+ *
+ */
+public class ModNBTTag
+{
+    private static int nextID = 0;
+
+    public final static String LOCATION_DIMENSION = "xm" + Integer.toUnsignedString(++nextID, Character.MAX_RADIX);
+    public final static String LOCATION_POSITION = "xm" + Integer.toUnsignedString(++nextID, Character.MAX_RADIX);
+
+}
---
+++ src/main/java/grondag/exotic_matter/world/CornerJoinFaceSelector.java
@@ -0,0 +1,34 @@
+package grondag.exotic_matter.world;
+
+import net.minecraft.util.EnumFacing;
+
+public class CornerJoinFaceSelector
+{
+    public final EnumFacing face;
+    
+    public final int faceCount;
+    public final CornerJoinFaceState[] faceJoins;
+    public final int[] joinIndex = new int[48];
+    
+    public CornerJoinFaceSelector(EnumFacing face, SimpleJoin baseJoinState)
+    {
+        this.face = face;
+        faceJoins = CornerJoinFaceState.find(face, baseJoinState).getSubStates();
+        this.faceCount = faceJoins.length;
+        
+        for(int i = 0; i < faceCount; i++)
+        {
+            joinIndex[faceJoins[i].ordinal()] = i;
+        }
+    }
+
+    public int getIndexFromNeighbors(NeighborBlocks<?>.NeighborTestResults tests)
+    {
+        return joinIndex[CornerJoinFaceState.find(face, tests).ordinal()];
+    }
+    
+    public CornerJoinFaceState getFaceJoinFromIndex(int index)
+    {
+        return faceJoins[index];
+    }
+}
