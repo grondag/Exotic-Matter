3fb6d9b84799a528147e2f2094652a905d4eb87a
grondag <grondag@gmail.com>
2018-07-02 08:12:08 -0700

    Improve shader support code

--- src/main/java/grondag/exotic_matter/model/render/Shaders.java
+++ src/main/java/grondag/exotic_matter/model/render/Shaders.java
@@ -1,17 +1,22 @@
 package grondag.exotic_matter.model.render;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.function.Consumer;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.io.IOUtils;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 import grondag.exotic_matter.ExoticMatter;
 import grondag.exotic_matter.varia.SimpleUnorderedArrayList;
+import net.minecraft.client.renderer.OpenGlHelper;
 
 /**
  * This code is loosely based on code from Botania, by Vaskii
@@ -23,10 +28,303 @@ public final class Shaders
     {
         // TODO: remove
         private int tick = 0;
-        private int glID = 0;
+        private int progID = 0;
         public final @Nullable String fragmentFileName;
         public final @Nullable String vertexFileName;
+        private final SimpleUnorderedArrayList<Uniform> uniforms = new SimpleUnorderedArrayList<>();
+        protected boolean isDirty = false;
         
+        public abstract class Uniform
+        {
+            private final String name;
+            protected int unifID = -1;
+            protected final @Nullable Consumer<Uniform> initializer;
+            
+            protected Uniform(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                this.name = name;
+                this.initializer = initializer;
+            }
+            
+            private void load(int programID)
+            {
+                this.unifID = OpenGlHelper.glGetUniformLocation(programID, name);
+                if(this.unifID == -1)
+                    ExoticMatter.INSTANCE.warn("Unable to find uniform %s in shader(s) %s, %s", this.name, Shader.this.vertexFileName, Shader.this.fragmentFileName);
+                else if(this.initializer != null)
+                    this.initializer.accept(this);
+            }
+            
+            protected abstract void upload();
+        }
+        
+        protected abstract class UniformFloat extends Uniform
+        {
+            protected final FloatBuffer uniformFloatBuffer;
+            
+            protected UniformFloat(String name, @Nullable Consumer<Uniform> initializer, int size)
+            {
+                super(name, initializer);
+                this.uniformFloatBuffer = BufferUtils.createFloatBuffer(size);
+            }
+        }
+        
+        public class Uniform1f extends UniformFloat
+        {
+            protected Uniform1f(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 1);
+            }
+
+            public void set(float value)
+            {
+                if(this.unifID == -1) return;
+                this.uniformFloatBuffer.position(0);
+                this.uniformFloatBuffer.put(0, value);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform1(this.unifID, this.uniformFloatBuffer);
+            }
+        }
+        
+        public class Uniform2f extends UniformFloat
+        {
+            protected Uniform2f(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 2);
+            }
+
+            public void set(float v0, float v1)
+            {
+                if(this.unifID == -1) return;
+                this.uniformFloatBuffer.position(0);
+                this.uniformFloatBuffer.put(0, v0);
+                this.uniformFloatBuffer.put(1, v1);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform2(this.unifID, this.uniformFloatBuffer);
+            }
+        }
+        
+        public class Uniform3f extends UniformFloat
+        {
+            protected Uniform3f(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 3);
+            }
+
+            public void set(float v0, float v1, float v2)
+            {
+                if(this.unifID == -1) return;
+                this.uniformFloatBuffer.position(0);
+                this.uniformFloatBuffer.put(0, v0);
+                this.uniformFloatBuffer.put(1, v1);
+                this.uniformFloatBuffer.put(2, v2);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform3(this.unifID, this.uniformFloatBuffer);
+            }
+        }
+        
+        public class Uniform4f extends UniformFloat
+        {
+            protected Uniform4f(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 4);
+            }
+
+            public void set(float v0, float v1, float v2, float v3)
+            {
+                if(this.unifID == -1) return;
+                this.uniformFloatBuffer.position(0);
+                this.uniformFloatBuffer.put(v0);
+                this.uniformFloatBuffer.put(v1);
+                this.uniformFloatBuffer.put(v2);
+                this.uniformFloatBuffer.put(v3);
+                this.uniformFloatBuffer.flip();
+                isDirty = true;                
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform4(this.unifID, this.uniformFloatBuffer);
+            }
+        }
+        
+        public Uniform1f uniform1f(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform1f result = new Uniform1f(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform2f uniform2f(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform2f result = new Uniform2f(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform3f uniform3f(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform3f result = new Uniform3f(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform4f uniform4f(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform4f result = new Uniform4f(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        protected abstract class UniformInt extends Uniform
+        {
+            protected final IntBuffer uniformIntBuffer;
+            
+            protected UniformInt(String name, @Nullable Consumer<Uniform> initializer, int size)
+            {
+                super(name, initializer);
+                this.uniformIntBuffer = BufferUtils.createIntBuffer(size);
+            }
+        }
+        
+        public class Uniform1i extends UniformInt
+        {
+            protected Uniform1i(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 1);
+            }
+
+            public void set(int value)
+            {
+                if(this.unifID == -1) return;
+                this.uniformIntBuffer.position(0);
+                this.uniformIntBuffer.put(0, value);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform1(this.unifID, this.uniformIntBuffer);
+            }
+        }
+        
+        public class Uniform2i extends UniformInt
+        {
+            protected Uniform2i(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 2);
+            }
+
+            public void set(int v0, int v1)
+            {
+                if(this.unifID == -1) return;
+                this.uniformIntBuffer.position(0);
+                this.uniformIntBuffer.put(0, v0);
+                this.uniformIntBuffer.put(1, v1);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform2(this.unifID, this.uniformIntBuffer);
+            }
+        }
+        
+        public class Uniform3i extends UniformInt
+        {
+            protected Uniform3i(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 3);
+            }
+
+            public void set(int v0, int v1, int v2)
+            {
+                if(this.unifID == -1) return;
+                this.uniformIntBuffer.position(0);
+                this.uniformIntBuffer.put(0, v0);
+                this.uniformIntBuffer.put(1, v1);
+                this.uniformIntBuffer.put(2, v2);
+                isDirty = true;
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform3(this.unifID, this.uniformIntBuffer);
+            }
+        }
+        
+        public class Uniform4i extends UniformInt
+        {
+            protected Uniform4i(String name, @Nullable Consumer<Uniform> initializer)
+            {
+                super(name, initializer, 4);
+            }
+
+            public void set(int v0, int v1, int v2, int v3)
+            {
+                if(this.unifID == -1) return;
+                this.uniformIntBuffer.position(0);
+                this.uniformIntBuffer.put(v0);
+                this.uniformIntBuffer.put(v1);
+                this.uniformIntBuffer.put(v2);
+                this.uniformIntBuffer.put(v3);
+                this.uniformIntBuffer.flip();
+                isDirty = true;                
+            }
+            
+            @Override
+            protected void upload()
+            {
+                OpenGlHelper.glUniform4(this.unifID, this.uniformIntBuffer);
+            }
+        }
+        
+        public Uniform1i uniform1i(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform1i result = new Uniform1i(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform2i uniform2i(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform2i result = new Uniform2i(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform3i uniform3i(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform3i result = new Uniform3i(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
+        
+        public Uniform4i uniform4i(String name, @Nullable Consumer<Uniform> initializer)
+        {
+            Uniform4i result = new Uniform4i(name, initializer);
+            this.uniforms.add(result);
+            return result;
+        }
         private Shader(@Nullable String vertexFileName, @Nullable String fragmentFileName)
         {
             this.fragmentFileName = fragmentFileName;
@@ -35,16 +333,21 @@ public final class Shaders
         
         private void load()
         {
-            if(glID != 0)
+            if(progID != 0)
                 this.release();
             
-            this.glID = createProgram(this.vertexFileName, this.fragmentFileName);
+            this.progID = createProgram(this.vertexFileName, this.fragmentFileName);
+            if(progID != 0)
+                this.uniforms.forEach(u -> u.load(progID));
+            
+            isDirty = true;
         }
+        
         private void release()
         {
-            if(glID == 0) return;
-            GL20.glDeleteProgram(glID);
-            glID = 0;
+            if(progID == 0) return;
+            OpenGlHelper.glDeleteProgram(progID);
+            progID = 0;
         }
         
         public void activate()
@@ -56,32 +359,19 @@ public final class Shaders
                 this.load();
             }
             
-            if(glID != 0)
-                GL20.glUseProgram(glID);
+            if(progID != 0)
+                OpenGlHelper.glUseProgram(progID);
+            
+            if(this.isDirty)
+            {
+                this.uniforms.forEach(u -> {if(u.unifID != -1) u.upload();});
+                this.isDirty = false;
+            }
         }
 
         public void deactivate()
         {
-            GL20.glUseProgram(0);
-        }
-        
-        // TODO: if going to call these frequently may need to retain location reference
-        public void setUniform(String name, int value)
-        {
-            if(glID == 0) return;
-            final int uid = GL20.glGetUniformLocation(glID, name);
-            if(uid != -1)
-                GL20.glUniform1i(uid, value);
-        }
-      
-        
-     // TODO: if going to call these frequently may need to retain location reference
-        public void setUniform(String name, float value)
-        {
-            if(glID == 0) return;
-            final int uid =  GL20.glGetUniformLocation(glID, name);
-            if(uid != -1)
-                GL20.glUniform1f(uid, value);
+            OpenGlHelper.glUseProgram(0);
         }
     }
     
@@ -106,28 +396,21 @@ public final class Shaders
     {
         int vertId = 0, fragId = 0, programID;
         if(vert != null)
-            vertId = createShader(vert, GL20.GL_VERTEX_SHADER);
+            vertId = createShader(vert, OpenGlHelper.GL_VERTEX_SHADER);
         if(frag != null)
-            fragId = createShader(frag, GL20.GL_FRAGMENT_SHADER);
+            fragId = createShader(frag, OpenGlHelper.GL_FRAGMENT_SHADER);
 
-        programID = GL20.glCreateProgram();
+        programID = OpenGlHelper.glCreateProgram();
         if(programID == 0)
             return 0;
 
         if(vert != null)
-            GL20.glAttachShader(programID, vertId);
+            OpenGlHelper.glAttachShader(programID, vertId);
         if(frag != null)
-            GL20.glAttachShader(programID, fragId);
+            OpenGlHelper.glAttachShader(programID, fragId);
 
-        GL20.glLinkProgram(programID);
-        if(GL20.glGetProgrami(programID, GL20.GL_LINK_STATUS) == GL11.GL_FALSE)
-        {
-            ExoticMatter.INSTANCE.error(getLogInfo(programID));
-            return 0;
-        }
-
-        GL20.glValidateProgram(programID);
-        if (GL20.glGetProgrami(programID, GL20.GL_VALIDATE_STATUS) == GL11.GL_FALSE)
+        OpenGlHelper.glLinkProgram(programID);
+        if(OpenGlHelper.glGetProgrami(programID, OpenGlHelper.GL_LINK_STATUS) == GL11.GL_FALSE)
         {
             ExoticMatter.INSTANCE.error(getLogInfo(programID));
             return 0;
@@ -136,28 +419,32 @@ public final class Shaders
         return programID;
     }
 
-    // TODO: use OpenGL helper to use appropriate methods
     private static int createShader(String filename, int shaderType)
     {
         int shader = 0;
         try
         {
-            shader = GL20.glCreateShader(shaderType);
+            shader = OpenGlHelper.glCreateShader(shaderType);
 
             if(shader == 0)
                 return 0;
 
-            GL20.glShaderSource(shader, readFileAsString(filename));
-            GL20.glCompileShader(shader);
+            @Nullable ByteBuffer source = readFileAsString(filename);
+            
+            if(source == null)
+                return 0;
+            
+            OpenGlHelper.glShaderSource(shader, source);
+            OpenGlHelper.glCompileShader(shader);
 
-            if (GL20.glGetProgrami(shader, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
+            if (OpenGlHelper.glGetProgrami(shader, OpenGlHelper.GL_COMPILE_STATUS) == GL11.GL_FALSE)
                 throw new RuntimeException("Error creating shader: " + getLogInfo(shader));
 
             return shader;
         }
         catch(Exception e)
         {
-            GL20.glDeleteShader(shader);
+            OpenGlHelper.glDeleteShader(shader);
             ExoticMatter.INSTANCE.error("Unable to create shader", e);
             return -1;
         }
@@ -165,19 +452,20 @@ public final class Shaders
 
     private static String getLogInfo(int obj)
     {
-        return GL20.glGetProgramInfoLog(obj, GL20.glGetProgrami(obj, GL20.GL_INFO_LOG_LENGTH));
+        return OpenGlHelper.glGetProgramInfoLog(obj, OpenGlHelper.glGetProgrami(obj, GL20.GL_INFO_LOG_LENGTH));
     }
 
-    private static String readFileAsString(String filename) throws Exception
+    private static @Nullable ByteBuffer readFileAsString(String filename) throws Exception
     {
         InputStream in = Shaders.class.getResourceAsStream(filename);
 
         if(in == null)
-            return "";
+            return null;
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8")))
-        {
-            return reader.lines().collect(Collectors.joining("\n"));
-        }
+        byte[] abyte = IOUtils.toByteArray(new BufferedInputStream(in));
+        ByteBuffer bytebuffer = BufferUtils.createByteBuffer(abyte.length);
+        bytebuffer.put(abyte);
+        bytebuffer.position(0);
+        return bytebuffer;
     }
 }
