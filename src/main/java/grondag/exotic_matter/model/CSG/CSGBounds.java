a53cf340fa05f6d5cab095607b9c19a75ea34fc5
grondag <grondag@gmail.com>
2018-11-30 19:12:30 -0800

    This belongs here

--- src/main/java/grondag/exotic_matter/model/CSG/CSGBounds.java
+++ src/main/java/grondag/exotic_matter/model/CSG/CSGBounds.java
@@ -1,11 +1,55 @@
 package grondag.exotic_matter.model.CSG;
 
 import grondag.exotic_matter.model.primitives.polygon.IPolygon;
+import grondag.exotic_matter.model.primitives.stream.IPolyStream;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.Vec3d;
 
 public class CSGBounds extends AxisAlignedBB
 {
+    public static CSGBounds getBounds(IPolyStream forPolygons)
+    {
+        float minX = Float.MAX_VALUE;
+        float minY = Float.MAX_VALUE;
+        float minZ = Float.MAX_VALUE;
+        float maxX = Float.MIN_VALUE;
+        float maxY = Float.MIN_VALUE;
+        float maxZ = Float.MIN_VALUE;
+        
+        if(forPolygons.size() > 0)
+        {
+            IPolygon reader = forPolygons.reader();
+            forPolygons.origin();
+
+            do
+            {
+                final int vCount = reader.vertexCount();
+                for(int i = 0; i < vCount; i++)
+                {
+                    final float x = reader.getVertexX(i);
+                    if(x < minX)  
+                        minX = x;
+                    else if(x > maxX)
+                        maxX = x;
+                    
+                    final float y = reader.getVertexY(i);
+                    if(y < minY)  
+                        minY = y;
+                    else if(y > maxY)
+                        maxY = y;
+                    
+                    final float z = reader.getVertexZ(i);
+                    if(z < minZ)  
+                        minZ = z;
+                    else if(z > maxZ)
+                        maxZ = z;
+                }
+            } while(forPolygons.next());
+        }
+
+        return new CSGBounds(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+    
     public CSGBounds(double x1, double y1, double z1, double x2, double y2, double z2)
     {
         super(x1, y1, z1, x2, y2, z2);
